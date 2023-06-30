4ad0129edc7faa6995ffa73283f91e6a1694da6f
Ritwik Das <53152719+devritwik@users.noreply.github.com>
2023-06-30 16:47:04 +0530

    Update IconButton.js
    Resume Updated

--- src/components/IconButton.js
+++ src/components/IconButton.js
@@ -3,5 +3,5 @@ import './IconButton.css'
 
 export default function IconButton({text}){
 
-    return (<a href="https://drive.google.com/file/d/1eEu8iPKuwYPlnzyBFPnf1GtWxsJmeWCU/view?usp=sharing" className='IconButton' target="_blank"><img src={DownloadIcon}/>{text}</a>)
+    return (<a href="https://drive.google.com/file/d/1iUkkv_B9n2LWFmf4CtAZYopoqNiwV7z_/view?usp=sharing" className='IconButton' target="_blank"><img src={DownloadIcon}/>{text}</a>)
 }
